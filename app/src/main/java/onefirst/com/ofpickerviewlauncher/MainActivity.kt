e39dadd80383e4a6b6411548a5767d12933b9cb7
ysw <onefirstdev1@gmail.com>
2023-01-14 14:01:20 +0900

    권한 추가

--- app/src/main/java/onefirst/com/ofpickerviewlauncher/MainActivity.kt
+++ app/src/main/java/onefirst/com/ofpickerviewlauncher/MainActivity.kt
@@ -1,9 +1,16 @@
 package onefirst.com.ofpickerviewlauncher
 
+import android.Manifest.permission.BLUETOOTH_CONNECT
+import android.Manifest.permission.BLUETOOTH_SCAN
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import onefirst.com.onefirstcomponentlib.OFPickerView
 import onefirst.com.onefirstcomponentlib.OfPickerViewDelegate
@@ -16,7 +23,16 @@ class MainActivity: AppCompatActivity(), OfPickerViewDelegate {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            requestMultiplePermissions.launch(arrayOf(BLUETOOTH_SCAN, BLUETOOTH_CONNECT))
+        }else {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            requestBluetooth.launch(enableBtIntent)
+        }
+
+        // example1
 //        OFPickerView pickerView = new OFPickerView(this);
+        // example2
         pickerView = findViewById(R.id.pickerview)
 
         findViewById<View>(R.id.btn_scan).setOnClickListener {
@@ -26,10 +42,27 @@ class MainActivity: AppCompatActivity(), OfPickerViewDelegate {
         }
 
         findViewById<View>(R.id.btn_open).setOnClickListener {
+            // example1
 //            pickerView.openDoor(0);
+            // example2
             pickerView.openSelectedDoor()
         }
     }
+
+    private var requestBluetooth = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            //granted
+        }else{
+            //deny
+        }
+    }
+
+    private val requestMultiplePermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                Log.d("Test", "${it.key} = ${it.value}")
+            }
+        }
 
     override fun onDestroy() {
         super.onDestroy()
